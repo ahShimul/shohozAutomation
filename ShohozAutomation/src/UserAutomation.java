745e96efda745cc5bfed0a9a5365b721c18dce23
Shimul133102 <ah.shimul04@gmail.com>
2019-03-30 18:28:58 +0600

    Add files via upload

--- ShohozAutomation/src/UserAutomation.java
+++ ShohozAutomation/src/UserAutomation.java
@@ -1,16 +1,20 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 import java.net.URL;
+import java.util.List;
 
-import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileBy;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.touch.offset.PointOption;
 
 public class UserAutomation {
-	AppiumDriver<WebElement> driver;
+	AndroidDriver driver;
 
 	 @Test
 	 public void setUp() throws Exception {
@@ -32,7 +36,7 @@ public class UserAutomation {
 	 //set the Launcher activity name of the app
 	 capability.setCapability("appActivity", "com.shohoz.rides.activities.splash.SplashActivity");
 	//driver object with new Url and Capabilities
-	 driver= new AndroidDriver<WebElement>(new URL("http://127.0.0.1:4723/wd/hub"),capability);
+	 driver= new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"),capability);
 	 WebDriverWait wait = new WebDriverWait(driver, 30);
 	 wait.until(ExpectedConditions.elementToBeClickable(By
 	         .id("com.android.packageinstaller:id/permission_allow_button")));
@@ -60,22 +64,57 @@ public class UserAutomation {
 	 
 	 driver.findElement(By.id("com.shohoz.rides:id/imageView_food_switch")).click();
 	 
+	// if(driver.findElement(By.id("com.shohoz.rides:id/navMenu"))==null)
+	//	 driver.findElement(By.id("com.shohoz.rides:id/imageView_food_switch")).click();
+	 
+	// wait.until(ExpectedConditions.elementToBeClickable(By
+	 //        .id("com.shohoz.rides:id/swipe_layout")));
+	 
+	// driver.findElement(By.id("com.shohoz.rides:id/navMenu")).click();
+	 
+	 
+	// wait.until(ExpectedConditions.elementToBeClickable(By
+	 //        .id("com.shohoz.rides:id/img_profile")));
+	 
+	// driver.findElement(By.id("com.shohoz.rides:id/img_profile")).click();
+	 
+	// Thread.sleep(2000);
+	// driver.navigate().back();
+	// driver.navigate().back();
+	// driver.navigate().back();
+	 
+	 
 	 wait.until(ExpectedConditions.elementToBeClickable(By
-	         .id("com.shohoz.rides:id/swipe_layout")));
+	         .id("com.shohoz.rides:id/textView_delivery_address")));
 	 
-	 driver.findElement(By.id("com.shohoz.rides:id/navMenu")).click();
+	 driver.findElement(By.id("com.shohoz.rides:id/textView_delivery_address")).click();
 	 
-	 
+	 Thread.sleep(5000);
+	 //wait.until(ExpectedConditions.elementToBeClickable(By.className("android.view.View")));
+	 	 
+	 TouchAction ta = new TouchAction(driver);
+     ta.press(PointOption.point(360, 640)).moveTo(PointOption.point(380,700)).release().perform();
+     
 	 wait.until(ExpectedConditions.elementToBeClickable(By
-	         .id("com.shohoz.rides:id/img_profile")));
-	 
-	 driver.findElement(By.id("com.shohoz.rides:id/img_profile")).click();
-	 
-	 
-	 
-	 Thread.sleep(10000);
+	         .id("com.shohoz.rides:id/button_set_location")));
+     driver.findElement(By.id("com.shohoz.rides:id/button_set_location")).click();
+     
+     Thread.sleep(5000);
+     
+     //TouchAction t = new TouchAction(driver);
+     //t.press(PointOption.point(360, 1200)).moveTo(PointOption.point(360,600)).release().perform();
+     MobileElement element = (MobileElement) driver.findElement(MobileBy.AndroidUIAutomator(
+				"new UiScrollable(new UiSelector().resourceId(\"com.shohoz.rides:id/swipe_layout\")).scrollIntoView("
+				+ "new UiSelector().textContains(\"Rice\"))"));
+  		 
+     element.click();		
+     
+    
+	  
+  
+    wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//android.widget.TextView[@resource-id='com.shohoz.rides:id/textView_categoryName' and contains(@text,'Tehari')]")));
+    
 	 driver.quit();
 	 }
 
 }
-
